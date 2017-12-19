8b06a76637431f6f0c5aaf2cee596219820a639d
Mark Crisostomo <mark@deviseinteractive.com>
2017-12-18 21:28:50 -0800

    adapter list and category list view

--- app/src/main/java/com/markcrisostomo/storeapp/Controller/MainActivity.kt
+++ app/src/main/java/com/markcrisostomo/storeapp/Controller/MainActivity.kt
@@ -2,12 +2,24 @@ package com.markcrisostomo.storeapp.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.markcrisostomo.storeapp.Model.Category
 import com.markcrisostomo.storeapp.R
+import com.markcrisostomo.storeapp.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories
+                )
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/markcrisostomo/storeapp/Model/Category.kt
+++ app/src/main/java/com/markcrisostomo/storeapp/Model/Category.kt
@@ -1,4 +1,8 @@
 package com.markcrisostomo.storeapp.Model
 
 
-class Category constructor(val title: String, val image: String)
+class Category constructor(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
